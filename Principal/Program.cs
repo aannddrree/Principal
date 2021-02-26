ef92b74119f2dee8c0e706c80868dcfecc2d2db0
unknown <aannddrree>
2021-02-26 12:36:03 -0300

    implantacao

---
+++ Principal/Program.cs
@@ -0,0 +1,26 @@
+﻿using Logical;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client cli = new Client()
+            {
+                Id = 1,
+                Nome = "andre",
+                Telefone = "16 99887744"
+            };
+
+            Console.WriteLine(Print.show(cli));
+            Console.ReadKey();
+        }
+    }
+}
